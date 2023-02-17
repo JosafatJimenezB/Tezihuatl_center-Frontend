01096c4472914410b2fc012b9f01f6516587ddcb
josafat <josa221001@gmail.com>
2023-02-16 22:41:08 -0600

    add: agregando boton

--- src/App.js
+++ src/App.js
@@ -7,19 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h2>It is {new Date().toLocaleTimeString()}</h2>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => {alert()}}>Hola</button>
       </header>
     </div>
   );
-}
+}setInterval(App, 10000);
+
 
 export default App;
